de375fa407f89285fc32c1cf3f0c158e13ca54d5
slii995 <it24103502@my.sliit.lk>
2025-09-18 17:07:50 +0530

    Added Teacher entity

--- Edu-Camp/Edu-Camp/src/main/java/com/example/Edu_Camp/models/Teacher.java
+++ Edu-Camp/Edu-Camp/src/main/java/com/example/Edu_Camp/models/Teacher.java
@@ -1,5 +1,25 @@
 package com.example.Edu_Camp.models;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name = "teachers")
 public class Teacher {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String subject;
+    private String phone;
+    private String address;
+
+    public Teacher() {}
+
+    public Teacher(String name,String email,String subject,String phone,String address){
+
+    }
 }
